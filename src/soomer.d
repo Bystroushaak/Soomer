61254f889197bd9d2860ba22f485662fec872ee7
Bystroushaak <bystrousak@kitakitsune.org>
2011-11-17 21:36:05 +0100

    Added function processConf() to soomer.d.
    This function takes string and returns associative array with configuration
    parsed from string.

--- src/soomer.d
+++ src/soomer.d
@@ -12,17 +12,68 @@
  *     http://creativecommons.org/licenses/by/3.0/
 */
 import std.stdio;
+import std.string;
+import std.file;
+
 
 /// See https://github.com/Bystroushaak for details
 import dhttpclient;
 import dhtmlparser;
-import dfakemailer;
+import fakemailer_api;
 
 
+
+const string EXAMPLE_CONF = import("example.conf");
+const string HELP_STR     = import("help.txt");
+const string VERSION_STR  = import("version.txt");
+
+const string CONF_PATH    = "~/.soomer/soomer.conf";
+
+
+/// Return associative array with configuration parsed from string
+string[string] processConf(string conf){
+	string[string] parsed_conf;
+	
+	foreach(line; conf.splitLines()){
+		if (line.indexOf("=") <= 0)
+			continue;
+		
+		string[] tmp = line.split("=");
+		
+		// remove whitespaces from key & val
+		tmp[0] = tmp[0].strip();
+		tmp[1] = tmp[1].strip();
+		
+		parsed_conf[tmp[0].toUpper()] = tmp[1];
+	}
+	
+	return parsed_conf;
+}
 
 
 int main(string[] args){
-	
+	writeln(processConf(EXAMPLE_CONF));
 	
 	return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
