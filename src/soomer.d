8106aa9d5ab5a705cbf9b8fd37556e30db8989bc
Bystroushaak <bystrousak@kitakitsune.org>
2011-11-17 20:04:34 +0100

    Created.

---
+++ src/soomer.d
@@ -0,0 +1,28 @@
+/**
+ * soomer.d
+ *
+ * Program ktery sleduje zadana vlakna na soomu a pokud v nich nastane zmena
+ * upozorni na ni emailem uzivatele.
+ * 
+ * Author:  Bystroushaak (bystrousak@kitakitsune.org)
+ * Date:    17.11.2011
+ * 
+ * Copyright: 
+ *     This work is licensed under a CC BY.
+ *     http://creativecommons.org/licenses/by/3.0/
+*/
+import std.stdio;
+
+/// See https://github.com/Bystroushaak for details
+import dhttpclient;
+import dhtmlparser;
+import dfakemailer;
+
+
+
+
+int main(string[] args){
+	
+	
+	return 0;
+}
